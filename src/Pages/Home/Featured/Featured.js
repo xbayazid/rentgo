9bec00a59106cfac59a4a5e7fc0255ce9196221d
rakib4485 <mdrakibulislam4485@gmail.com>
2023-07-26 01:38:26 +0600

    featear added

--- src/Pages/Home/Featured/Featured.js
+++ src/Pages/Home/Featured/Featured.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import condo from '../../../assets/images/condo.png';
+import apartment from '../../../assets/images/apartment.png';
+import villa from '../../../assets/images/villa.png';
+import home from '../../../assets/images/home.png';
 
 const Featured = () => {
     return (
-        <div>
-            <h1>Featured</h1>
+        <div className='w-[80%] mx-auto grid grid-cols-4 gap-6 my-12'>
+            <div className='border text-center flex justify-center items-center flex-col p-5 hover:shadow-xl duration-500 rounded-md'>
+                <img src={condo} alt="" />
+                <h5 className='text-purple-600 text-xl'>Condominium</h5>
+                <p>45 Listing</p>
+            </div>
+            <div className='border text-center flex justify-center items-center flex-col p-5 hover:shadow-xl duration-500 rounded-md'>
+                <img src={apartment} alt="" />
+                <h5 className='text-purple-600 text-xl'>Apartment</h5>
+                <p>45 Listing</p>
+            </div>
+            <div className='border text-center flex justify-center items-center flex-col p-5 hover:shadow-xl duration-500 rounded-md'>
+                <img src={home} alt="" />
+                <h5 className='text-purple-600 text-xl'>Home</h5>
+                <p>45 Listing</p>
+            </div>
+            <div className='border text-center flex justify-center items-center flex-col p-5 hover:shadow-xl duration-500 rounded-md'>
+                <img src={villa} alt="" />
+                <h5 className='text-purple-600 text-xl'>Villa</h5>
+                <p>45 Listing</p>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import heroBg from '../../../assets/images/home-bg.png';
 
 const Hero = () => {
     return (
-        <div>
-            <h1>Hero</h1>
+        <div className='w-full h-screen -mt-12' style={{backgroundImage: `url(${heroBg})`, backgroundPosition:"center", backgroundSize:"cover", backgroundRepeat:"no-repeat"}}>
+            <div className="flex justify-center items-center h-full flex-col">
+                <h1 className='text-5xl font-semibold capitalize text-white'>Let us guide you home</h1>
+                <div className='flex gap-4 mt-3 bg-gray-300 p-5 w-[80%] justify-between outline-none focus:outline-none visited:outline-none'>
+                    <input type="text" className='w-2/3 px-3' placeholder='Enter Property,Location,Landmark...' />
+                    <select name="" id="" className='px-7 py-2'>
+                        <option value="Select City">Select City</option>
+                        <option value="Dhaka">Dhaka</option>
+                        <option value="Cumilla">Cumilla</option>
+                        <option value="Chittagong">Chittagong</option>
+                    </select>
+                    <button className='px-7 py-3 bg-purple-800 text-white rounded'>Search</button>
+                </div>
+            </div>
+            
         </div>
     );
 };
