95dff6ce9469d01e53500589f5ad6897adea71b2
xbayazid <bayazid9006@gmail.com>
2023-07-25 23:26:27 +0600

    find properties done

--- src/Pages/Home/Choose/Choose.js
+++ src/Pages/Home/Choose/Choose.js
@@ -2,8 +2,70 @@ import React from 'react';
 
 const Choose = () => {
     return (
-        <div>
-            <h1>Choose</h1>
+        <div className='my-5 mx-5'>
+            <div className='overflow-y-hidden'>
+    <div className="xl:mx-auto xl:container  xl:px-20 md:px-6 px-4 py-12">
+      <div className="lg:flex items-center justify-center lg:space-x-12 2xl:space-x-6">
+        <div className>
+          <p className="lg:text-4xl text-3xl font-extrabold leading-9 text-gray-800">Why choose us</p>
+          <p className="text-lg leading-7 text-gray-600 mt-4 xl:w-7/12 w-full">Whether article spirits new her covered hastily sitting her. Money witty books nor son add</p>
+          <div className="lg:hidden lg:w-3/5 xl:w-3/5 w-full lg:mt-0 mt-6">
+            <img src="https://i.ibb.co/SKLJ7WX/austin-distel-jp-Hw8ndw-J-Q-unsplash-1.png" alt="ongoing meeting" className="w-full obejct-fit object-center object-fill h-full" />
+          </div>
+          <div className="mt-6 md:mt-10 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6 lg:mt-6 2xl:mt-12">
+            <div className="flex items-center">
+              <div className="w-16 h-16 relative">
+                <div className=" flex-shrink-0 z-20  w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_6_Svg1.svg" alt="clock" />
+                </div>
+              </div>
+              <div className="flex items-start flex-col ml-6 pt-8">
+                <h2 className="text-lg font-semibold leading-4 text-gray-800">Time Prompt</h2>
+                <p className="lg:w-40 2xl:w-52 text-base leading-6 mt-2 text-gray-600">Whether article spirits new her covered.</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className="w-16 h-16 relative">
+                <div className=" flex-shrink-0 z-20  w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_6_Svg2.svg" alt="Friendly" />
+                </div>
+              </div>
+              <div className="flex items-start flex-col ml-6 pt-8">
+                <h2 className="text-lg font-semibold leading-4 text-gray-800">User Friendly</h2>
+                <p className="lg:w-40 2xl:w-52 text-base leading-6 mt-2 text-gray-600">Whether article spirits new her covered.</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className="w-16 h-16 relative">
+                <div className=" flex-shrink-0 z-20  w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_6_Svg3.svg" alt="Creative" />
+                </div>
+              </div>
+              <div className="flex items-start flex-col ml-6 pt-8">
+                <h2 className="text-lg font-semibold leading-4 text-gray-800">Creative Ideas</h2>
+                <p className="lg:w-40 2xl:w-52 text-base leading-6 mt-2 text-gray-600">Whether article spirits new her covered.</p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className="w-16 h-16 relative">
+                <div className=" flex-shrink-0 z-20  w-16 h-16 flex items-center justify-center mt-2 mr-3">
+                  <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/feature_6_Svg4.svg" alt="Achievments" />
+                </div>
+              </div>
+              <div className="flex items-start flex-col ml-6 pt-8">
+                <h2 className="text-lg font-semibold leading-4 text-gray-800">Achievments</h2>
+                <p className="lg:w-40 2xl:w-52 text-base leading-6 mt-2 text-gray-600">Whether article spirits new her covered.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:block lg:w-3/5 xl:w-3/5 w-full lg:mt-0 mt-6">
+          <img src="https://i.ibb.co/SKLJ7WX/austin-distel-jp-Hw8ndw-J-Q-unsplash-1.png" alt="ongoing meeting" className="w-full obejct-fit object-center object-fill h-full" />
+        </div>
+      </div>
+    </div>
+  </div>
+
         </div>
     );
 };
--- src/Pages/Home/FindProperties/FindProperties.js
+++ src/Pages/Home/FindProperties/FindProperties.js
@@ -1,9 +1,195 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { HiOutlineLocationMarker, HiOutlineCheckCircle, HiOutlineStar } from "react-icons/hi";
 
 const FindProperties = () => {
     return (
-        <div>
-            <h1>FindProperties</h1>
+        <div className='my-5'>
+            <h1 className='text-center text-2xl font-semibold'>Find Properties</h1>
+            <div className="my-6 mx-8">
+                <div className="grid gap-5 lg:grid-cols-3">
+                    <div>
+                        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                            <img
+                                className="w-full"
+                                src="https://i.ibb.co/xz0JFq6/property-1.jpg" // Replace this URL with your actual image URL
+                                alt="Card"
+                            />
+                            <div className="px-6 py-4">
+                                <Link to='' className="text-xl hover:text-blue-700">Villa on Hartford</Link>
+                                <div className='mt-3'>
+                                    <p className='flex items-center gap-2'><HiOutlineLocationMarker className='text-blue-800'/><span className='text-gray-600'>2854 Meadow View Drive, Hartford, USA</span></p>
+                                    <div className="my-2">
+                                        <div className="grid grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>4 Bedroom</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2142 sq ft</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>3 Bath</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2 Garage</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div>
+                                        <div className="grid lg:grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineStar className='text-orange-300'/>4.5</p>
+                                            <p className='text-xl font-medium text-blue-800'>$200</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                            <img
+                                className="w-full"
+                                src="https://i.ibb.co/xz0JFq6/property-1.jpg" // Replace this URL with your actual image URL
+                                alt="Card"
+                            />
+                            <div className="px-6 py-4">
+                                <Link to='' className="text-xl hover:text-blue-700">Villa on Hartford</Link>
+                                <div className='mt-3'>
+                                    <p className='flex items-center gap-2'><HiOutlineLocationMarker className='text-blue-800'/><span className='text-gray-600'>2854 Meadow View Drive, Hartford, USA</span></p>
+                                    <div className="my-2">
+                                        <div className="grid grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>4 Bedroom</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2142 sq ft</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>3 Bath</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2 Garage</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div>
+                                        <div className="grid lg:grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineStar className='text-orange-300'/>4.5</p>
+                                            <p className='text-xl font-medium text-blue-800'>$200</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                            <img
+                                className="w-full"
+                                src="https://i.ibb.co/xz0JFq6/property-1.jpg" // Replace this URL with your actual image URL
+                                alt="Card"
+                            />
+                            <div className="px-6 py-4">
+                                <Link to='' className="text-xl hover:text-blue-700">Villa on Hartford</Link>
+                                <div className='mt-3'>
+                                    <p className='flex items-center gap-2'><HiOutlineLocationMarker className='text-blue-800'/><span className='text-gray-600'>2854 Meadow View Drive, Hartford, USA</span></p>
+                                    <div className="my-2">
+                                        <div className="grid grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>4 Bedroom</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2142 sq ft</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>3 Bath</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2 Garage</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div>
+                                        <div className="grid lg:grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineStar className='text-orange-300'/>4.5</p>
+                                            <p className='text-xl font-medium text-blue-800'>$200</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                            <img
+                                className="w-full"
+                                src="https://i.ibb.co/xz0JFq6/property-1.jpg" // Replace this URL with your actual image URL
+                                alt="Card"
+                            />
+                            <div className="px-6 py-4">
+                                <Link to='' className="text-xl hover:text-blue-700">Villa on Hartford</Link>
+                                <div className='mt-3'>
+                                    <p className='flex items-center gap-2'><HiOutlineLocationMarker className='text-blue-800'/><span className='text-gray-600'>2854 Meadow View Drive, Hartford, USA</span></p>
+                                    <div className="my-2">
+                                        <div className="grid grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>4 Bedroom</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2142 sq ft</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>3 Bath</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2 Garage</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div>
+                                        <div className="grid lg:grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineStar className='text-orange-300'/>4.5</p>
+                                            <p className='text-xl font-medium text-blue-800'>$200</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                            <img
+                                className="w-full"
+                                src="https://i.ibb.co/xz0JFq6/property-1.jpg" // Replace this URL with your actual image URL
+                                alt="Card"
+                            />
+                            <div className="px-6 py-4">
+                                <Link to='' className="text-xl hover:text-blue-700">Villa on Hartford</Link>
+                                <div className='mt-3'>
+                                    <p className='flex items-center gap-2'><HiOutlineLocationMarker className='text-blue-800'/><span className='text-gray-600'>2854 Meadow View Drive, Hartford, USA</span></p>
+                                    <div className="my-2">
+                                        <div className="grid grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>4 Bedroom</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2142 sq ft</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>3 Bath</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2 Garage</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div>
+                                        <div className="grid lg:grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineStar className='text-orange-300'/>4.5</p>
+                                            <p className='text-xl font-medium text-blue-800'>$200</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                            <img
+                                className="w-full"
+                                src="https://i.ibb.co/xz0JFq6/property-1.jpg" // Replace this URL with your actual image URL
+                                alt="Card"
+                            />
+                            <div className="px-6 py-4">
+                                <Link to='' className="text-xl hover:text-blue-700">Villa on Hartford</Link>
+                                <div className='mt-3'>
+                                    <p className='flex items-center gap-2'><HiOutlineLocationMarker className='text-blue-800'/><span className='text-gray-600'>2854 Meadow View Drive, Hartford, USA</span></p>
+                                    <div className="my-2">
+                                        <div className="grid grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>4 Bedroom</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2142 sq ft</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>3 Bath</p>
+                                            <p className='flex items-center gap-2'><HiOutlineCheckCircle/>2 Garage</p>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div>
+                                        <div className="grid lg:grid-cols-2">
+                                            <p className='flex items-center gap-2'><HiOutlineStar className='text-orange-300'/>4.5</p>
+                                            <p className='text-xl font-medium text-blue-800'>$200</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
