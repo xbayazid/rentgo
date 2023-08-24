4779b97fa21be4a15a4051c99a2446de2129200a
rakib4485 <mdrakibulislam4485@gmail.com>
2023-08-25 00:08:29 +0600

    details banner added

---
+++ src/Pages/PropertyDetails/PropertyDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropertyDetailsBanner from './PropertyDetailsBanner/PropertyDetailsBanner';
+
+const PropertyDetails = () => {
+    return (
+        <div>
+            <PropertyDetailsBanner/>
+        </div>
+    );
+};
+
+export default PropertyDetails;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import Signup from "../Pages/Signup/Signup";
+import PropertyDetails from "../Pages/PropertyDetails/PropertyDetails";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
             {
                 path: '/signup',
                 element: <Signup/>
+            },
+            {
+                path: '/propertyDetails',
+                element: <PropertyDetails/>
             }
         ]
     }
