0f71e4df805012817b1223f40576405c0bfc7642
rakib4485 <mdrakibulislam4485@gmail.com>
2023-08-25 23:15:20 +0600

    map added

---
+++ src/components/Map/Map.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+
+const Map = () => {
+    const defaultProps = {
+        center: {
+          lat: 23.8041,
+          lng: 90.4152
+        },
+        zoom: 11
+      };
+    return (
+        // Important! Always set the container height explicitly
+    <div style={{ height: '500px', width: '100%' }}>
+    <GoogleMapReact
+      bootstrapURLKeys={{ key: "" }}
+      defaultCenter={defaultProps.center}
+      defaultZoom={defaultProps.zoom}
+    >
+      <AnyReactComponent
+        lat={23.8041}
+        lng={90.4152}
+        text="My Marker"
+      />
+    </GoogleMapReact>
+  </div>
+    );
+};
+
+export default Map;
